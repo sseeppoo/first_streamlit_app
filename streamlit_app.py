ca3f53b1a7f2cc93b2e6be56069ef464ab4b1220
sseeppoo <37580268+sseeppoo@users.noreply.github.com>
2023-11-07 22:08:41 +0100

    Update streamlit_app.py
    import pandas 
    import file from s3 and loaded into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
@@ -8,5 +10,4 @@ streamlit.text('🐔 Hard-Bolied Free-Range EGG')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
+streamlit.dataframe(my_fruit_list)
